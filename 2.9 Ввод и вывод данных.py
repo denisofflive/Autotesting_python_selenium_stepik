6f93f491c2fe1a29cefacac7bc0f9e4fb2e8bb2b
Denis Denisov <denisofflive@mail.ru>
2023-07-25 20:10:35 +0300

    2.9 Ввод и вывод данных

---
+++ 2.9 Ввод и вывод данных.py
@@ -0,0 +1,21 @@
+# var = input()
+# print(var)
+# # type указывает какой у нас тип данных, в данном случае у нас строка (string)
+# print(type(var))
+
+# var = int(input())
+# print(var)
+# # type указывает какой у нас тип данных, в данном случае у нас числовой (int)
+# print(type(var))
+
+# Пример сложения с числовым типом
+# var_1 = int(input())
+# var_2 = int(input())
+# result = var_1 + var_2
+# print(result)
+
+# Пример сложения со строковым типом
+var_1 = input()
+var_2 = input()
+result = var_1 + var_2
+print("Меня зовут : " + var_1 + " " + var_2)
