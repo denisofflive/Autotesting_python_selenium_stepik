3587539ef610163fe13e684f9874bad7309d413b
Denis Denisov <denisofflive@mail.ru>
2023-07-25 20:09:26 +0300

    2.5 Числовые типы данных

---
+++ 2.5 Числовые типы данных.py
@@ -0,0 +1,37 @@
+# Деление с указание типа данных в результате
+# num_1 = 50
+# num_2 = 8
+# result = (num_1 / num_2)
+# print(int(result))
+
+# Остаток от деления
+# num_1 = 50
+# num_2 = 8
+# result = (num_1 % num_2)
+# print(int(result))
+"""
+8*6=48
+50-48=2
+"""
+
+# Определения правда/ложь (True/Boolean)
+
+# num_1 = 10
+# num_2 = 5
+# result = (num_1 % num_2)
+# print(num_1 == num_2)
+# print(num_1 > num_2)
+
+#Округление результата
+
+# num_1 = 10
+# num_2 = 3
+# result = (num_1 / num_2)
+# print(round(result))
+
+# с плавающей точкой
+# num_1 = float(10.5)
+# print(num_1)
+# num_2 = 10
+# result = num_1 + num_2
+# print(result)
