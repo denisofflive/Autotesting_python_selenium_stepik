38b9dfdcb42156adcd1fe0ae0014d8765d8e46d3
Denis Denisov <denisofflive@mail.ru>
2023-07-25 20:09:41 +0300

    2.6 Строчные типы данных

---
+++ 2.6 Строчные типы данных.py
@@ -0,0 +1,27 @@
+# str_1 = "Hello, "
+# str_2 = "world!"
+# result = str_1 + str_2
+# print(result)
+
+# str_1 = "три"
+# str_2 = 5
+# result = "I don't love you"
+# print(result)
+
+# # Вывод через тип данных int
+# var_1 = int("3")
+# var_2 = int("5")
+# result = var_1 + var_2
+# print(result)
+
+#Конкатенация данных
+
+# var_1 = "Hello"
+# var_2 = "World!"
+# result = var_1 + var_2
+# print(var_1 + ", " + var_2)
+
+var_1 = 5
+var_2 = 10
+result = var_1 + var_2
+print("Результат: " + str(result))
