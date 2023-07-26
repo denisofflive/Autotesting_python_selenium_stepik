ceeebd63732351926dd6a3ed3049b2ca9bad587b
Denis Denisov <denisofflive@mail.ru>
2023-07-26 17:06:10 +0300

    2.24 Работа с исключениями. Конструкция Try&Except

---
+++ 02. Базовый курс программирования/2.24 Работа с исключениями. Конструкция Try&Except.py
@@ -0,0 +1,14 @@
+a = int(input("Введите первое значение: "))
+b = int(input("Введите второе значение: "))
+
+try:
+    result = int(a / b)
+except ZeroDivisionError:
+    result = 0
+    print("На 0 делить нельзя")
+print("Результат : " + str(result))
+
+result_2 = a + b
+print(result_2)
+# ZeroDivisionError
+
