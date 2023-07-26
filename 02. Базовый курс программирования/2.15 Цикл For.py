66bed6b5fe2ae619b002d69b75af7f1c4c2b7fe8
Denis Denisov <denisofflive@mail.ru>
2023-07-26 13:00:10 +0300

    2.15 Цикл For

---
+++ 02. Базовый курс программирования/2.15 Цикл For.py
@@ -0,0 +1,46 @@
+# Создаём переменную students, в которую вносим список студентов
+# students = ["Alex", "Ivan", "Olga", "Semen", "Igor", "Svetlana"]
+
+"""
+Создаём цикл с переменной f,
+которая будет обращаться к каждому элементу нашего списка по очереди
+и указали из какого списка брать (из students).
+Цикл for проходит по нашему списку и каждый элемент присваивает 
+в качестве значения к переменной f и потом выводим на экран.
+P.S.название переменной может быть любой, не обязательно f)
+"""
+# for f in students:
+#     print(f)
+
+# Вариант 2 с переменной var
+# students = ["Alex", "Ivan", "Olga", "Semen", "Igor", "Svetlana"]
+#
+# for f in students:
+#     # используем конкатенацию строк
+#     var = "Инженер " + f
+#     print(var)
+#
+
+# Вариант 3 с использованием if
+# students = ["Alex", "Ivan", "Olga", "Semen", "Igor", "Svetlana"]
+#
+# for f in students:
+#     if f == "Olga":
+#         var = "Инженер " + f
+#         print(var)
+
+# Вариант 4 с использованием списка
+# students = ["Alex", "Ivan", "Olga", "Semen", "Igor", "Svetlana"]
+#
+# for f in students[:3]: # вывела 3 первых элемента из нашего списка
+# # for f in students[3:]: # вывела 3 последних элемента из нашего списка
+# # for f in students[1:4]: # вывела с 1 до 4 элемента из нашего списка
+#     print(f)
+
+
+# Вариант 4 с использованием len
+students = ["Alex", "Denis", "Yan", "Semen", "Igor", "Svetlana"]
+
+for f in students:
+    # выведет количество символов в каждом элементе
+    print(len(f))
