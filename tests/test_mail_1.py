8fd6a3561a1d1e06c8d7bc9e4f91dae061e61afb
denis <denisofflive@mail.ru>
2023-08-25 17:54:48 +0300

    6.4 Знакомство с параметром scope в PyTest

--- tests/test_mail_1.py
+++ tests/test_mail_1.py
@@ -1,16 +1,7 @@
-import pytest
 
-
-@pytest.fixture()
-def set_up():
-    print("Вход в систему выполнен")
-    yield
-    print("Выход из системы")
-
-
-def test_sending_mail_1(set_up):
+def test_sending_mail_1(set_up, some):
     print("Письмо 1 отправлено")
 
 
-def test_sending_mail_2(set_up):
+def test_sending_mail_2(set_up, some):
     print("Письмо 2 отправлено")
