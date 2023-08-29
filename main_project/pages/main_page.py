9e89b692c568a1da55e14c9212a00138f1f9cc04
denis <denisofflive@mail.ru>
2023-08-29 15:12:04 +0300

    7.11 Создание нескольких тестовых методов в одном файле

--- main_project/pages/main_page.py
+++ main_project/pages/main_page.py
@@ -14,6 +14,8 @@ class Main_page(Base):
     #Locators
 
     select_product_1 = "#add-to-cart-sauce-labs-backpack"
+    select_product_2 = "#add-to-cart-sauce-labs-bolt-t-shirt"
+    select_product_3 = "#add-to-cart-sauce-labs-fleece-jacket"
     cart = "#shopping_cart_container"
     menu = "#react-burger-menu-btn"
     link_about = '#about_sidebar_link'
@@ -23,6 +25,12 @@ class Main_page(Base):
 
     def get_select_product_1(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.select_product_1)))
+
+    def get_select_product_2(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.select_product_2)))
+
+    def get_select_product_3(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.select_product_3)))
 
     def get_cart(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.cart)))
@@ -39,6 +47,14 @@ class Main_page(Base):
         self.get_select_product_1().click()
         print("Click Select Product 1")
 
+    def click_select_product_2(self):
+        self.get_select_product_2().click()
+        print("Click Select Product 2")
+
+    def click_select_product_3(self):
+        self.get_select_product_3().click()
+        print("Click Select Product 3")
+
     def click_cart(self):
         self.get_cart().click()
         print("Click Cart")
@@ -54,9 +70,19 @@ class Main_page(Base):
     # Methods - наши методы (шаги)
 
 
-    def select_product(self):
+    def select_products_1(self):
         self.get_current_url()
         self.click_select_product_1()
+        self.click_cart()
+
+    def select_products_2(self):
+        self.get_current_url()
+        self.click_select_product_2()
+        self.click_cart()
+
+    def select_products_3(self):
+        self.get_current_url()
+        self.click_select_product_3()
         self.click_cart()
 
     def select_menu_about(self):
--- main_project/tests/test_buy_product.py
+++ main_project/tests/test_buy_product.py
@@ -1,10 +1,7 @@
 import time
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from main_project.pages.cart_page import Cart_page
 from main_project.pages.client_information_page import Client_inforamtion_page
 from main_project.pages.finish_page import Finish_page
@@ -12,32 +9,75 @@ from main_project.pages.login_page import Login_page
 from main_project.pages.main_page import Main_page
 from main_project.pages.payment_page import Payment_page
 
-
-def test_buy_product():
+@pytest.mark.run(order=2)
+def test_buy_product_1():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     g = Service()
     driver = webdriver.Chrome(options=options, service=g)
 
-    print("Start Test")
+    print("Start Test 1")
 
     login = Login_page(driver)
     login.authorization()
 
     mp = Main_page(driver)
-    mp.select_product()
+    mp.select_products_1()
 
     cp = Cart_page(driver)
     cp.click_checkout_button()
 
-    cip = Client_inforamtion_page(driver)
-    cip.input_information()
+    # cip = Client_inforamtion_page(driver)
+    # cip.input_information()
+    #
+    # p = Payment_page(driver)
+    # p.click_finish_button()
+    #
+    # f = Finish_page(driver)
+    # f.finish()
 
-    p = Payment_page(driver)
-    p.click_finish_button()
+    print('Finish Test 1')
+    time.sleep(1)
+    driver.close()
+@pytest.mark.run(order=3)
+def test_buy_product_2():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    g = Service()
+    driver = webdriver.Chrome(options=options, service=g)
 
-    f = Finish_page(driver)
-    f.finish()
+    print("Start Test 2")
 
+    login = Login_page(driver)
+    login.authorization()
+
+    mp = Main_page(driver)
+    mp.select_products_2()
+
+    cp = Cart_page(driver)
+    cp.click_checkout_button()
+
+    print('Finish Test 2')
+    time.sleep(1)
+    driver.close()
+@pytest.mark.run(order=1)
+def test_buy_product_3():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    g = Service()
+    driver = webdriver.Chrome(options=options, service=g)
+
+    print("Start Test 3")
+
+    login = Login_page(driver)
+    login.authorization()
+
+    mp = Main_page(driver)
+    mp.select_products_3()
+
+    cp = Cart_page(driver)
+    cp.click_checkout_button()
+
+    print('Finish Test 3')
     time.sleep(1)
     driver.close()
